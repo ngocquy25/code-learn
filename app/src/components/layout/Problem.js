39de24b76089a92b44f845836b588c18876523cc
ngocquy25 <quy.nguyen.jirou@gmail.com>
2022-01-24 15:25:56 +0700

    add post data

--- app/src/components/layout/Problem.js
+++ app/src/components/layout/Problem.js
@@ -2,14 +2,12 @@ import React from "react";
 import '../../styles/styles.css';
 
 function Problem({ question }){
-    // const { question_id, description } = question;
-
     return(
-        <div class="problem">
+        <div className="problem">
             {question.map(quest => (
-                <div>
-                    <h1 class="quest-title">Bài toán {quest.question_id}</h1>
-                    <p class="content">{quest.description}</p>
+                <div key={quest.question_id}>
+                    <h1 className="quest-title">Bài toán {quest.question_id}</h1>
+                    <p className="content">{quest.description.split('\\n').join('\n')}</p>
                 </div>
             ))}
         </div>
